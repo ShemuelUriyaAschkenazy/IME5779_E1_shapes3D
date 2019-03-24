ae74e8531ecbec11b006a8a6890d58c38a3bf3ac
shimonbalsam <46459891+shimonbalsam@users.noreply.github.com>
2019-03-24 22:52:51 +0200

    shichlul vetor2

--- src/primitives/Vector.java
+++ src/primitives/Vector.java
@@ -5,5 +5,30 @@ public class Vector {
     Coordinate _y;
     Coordinate _z;
 
+    public Vector(Coordinate x, Coordinate y,Coordinate z) {
+        _x=new Coordinate(x);
+        _y=new Coordinate(y);
+        _z=new Coordinate(z);
 
+    }
+
+    public Vector(Vector other) {
+        _x=other.getX();
+        _y=other.getY();
+        _z=other.getZ();
+    }
+
+    /************** Getters/Setters *******/
+    public Coordinate getX() {
+        return new Coordinate(_x);
+    }
+    public Coordinate getY() {
+        return new Coordinate(_y);
+    }
+    public Coordinate getZ() {
+        return new Coordinate(_z);
+    }
+    public Coordinate scale(double num) {
+        return new Vector(new Coordinate(uscale(_x._coord, num)),new Coordinate(uscale(_y, num)),new Coordinate(uscale(_z, num)));
+    }
 }
