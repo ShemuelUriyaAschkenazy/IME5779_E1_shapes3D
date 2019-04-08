ce63d1aee3d5f0c5c73270d8f50ab82b234bf2b6
שמואל אוריה אשכנזי <uriyaas@gmail.com>
2019-04-08 15:40:01 +0300

    מימוש נקודת חיתוך במחלקת כדור

--- src/geometries/Sphere.java
+++ src/geometries/Sphere.java
@@ -2,7 +2,11 @@ package geometries;
 
 
 import primitives.Point3D;
+import primitives.Ray;
 import primitives.Vector;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Sphere extends RadialGeometry implements Geometry{
 
@@ -28,5 +32,24 @@ public class Sphere extends RadialGeometry implements Geometry{
     public Vector getNormal(Point3D point3D) throws Exception {
         Vector vectorNormal = new Vector(point3D.subtract(this.center));
         return vectorNormal.normalize();
+    }
+
+    @Override
+    public List<Point3D> findIntersections(Ray ray) throws Exception {
+        Vector u= center.subtract(ray.getPoint());
+        double uLength= u.length();
+        double DistTilHalf= ray.getVector().dotProduct(u);
+        double d=Math.sqrt(uLength*uLength - (DistTilHalf*DistTilHalf));
+        double halfChord=Math.sqrt(_radius*_radius-(d*d));
+
+        double t1=DistTilHalf+halfChord;
+        double t2=DistTilHalf-halfChord;
+
+        List<Point3D> intersectionsPoints= new ArrayList<Point3D>();
+        if (t1>=0)
+            intersectionsPoints.add(ray.getPoint().add(ray.getVector().scale(t1)));
+        if (t2>=0)
+            intersectionsPoints.add(ray.getPoint().add(ray.getVector().scale(t2)));
+        return intersectionsPoints;
     }
 }
