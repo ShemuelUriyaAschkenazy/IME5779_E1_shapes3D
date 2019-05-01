9ad4743a1c4d765897f02e1e3a04c17b6eba2e6f
shimonbalsam <46459891+shimonbalsam@users.noreply.github.com>
2019-05-01 17:17:56 +0300

    javadoc at point3d

--- src/primitives/Point3D.java
+++ src/primitives/Point3D.java
@@ -3,14 +3,31 @@ package primitives;
 
 import com.sun.org.apache.bcel.internal.generic.NEW;
 
+/**
+ * contain 3 coordinate.
+ * The class has 3 constructors:
+ * a. get 3 coordinates
+ * b. get 3 double numbers.
+ * c. copy constructor.
+ */
 public final class Point3D {
 
+
+    /**
+     * const point3 that all coordinats is 0
+     */
     final public static Point3D ZERO = new Point3D(new Coordinate(0), new Coordinate(0), new Coordinate(0));
 
     private Coordinate _x;
     private Coordinate _y;
     private Coordinate _z;
 
+    /**
+     * constructor get 3 coordinates
+     * @param x is coordinate
+     * @param y is coordinate
+     * @param z is coordinate
+     */
     /* ********* Constructors ********** */
     public Point3D(Coordinate x, Coordinate y, Coordinate z) {
         _x = new Coordinate(x);
@@ -18,12 +35,22 @@ public final class Point3D {
         _z = new Coordinate(z);
     }
 
+    /**
+     * constructor get 3 double numbers.
+     * @param x is double number
+     * @param y is double number
+     * @param z is double number
+     */
     public Point3D(double x, double y, double z) {
         _x = new Coordinate(x);
         _y = new Coordinate(y);
         _z = new Coordinate(z);
     }
 
+    /**
+     * constructor get point and copy it to new point
+     * @param other is point3D
+     */
     public Point3D(Point3D other) {
         _x = new Coordinate(other._x);
         _y = new Coordinate(other._y);
@@ -34,18 +61,25 @@ public final class Point3D {
     /* ************* Getters/Setters ****** */
 
     /**
-     * asd asdf sdf dsf
-     *
-     * @return fdd vf
+     *function get the x coordinate
+     * @return  x coordinate
      */
     public Coordinate getX() {
         return _x;
     }
 
+    /**
+     * function get the x coordinate
+     * @return y coordinate
+     */
     public Coordinate getY() {
         return _y;
     }
 
+    /**
+     * function get the x coordinate
+     * @return z coordinate
+     */
     public Coordinate getZ() {
         return _z;
     }
@@ -60,6 +94,9 @@ public final class Point3D {
         return _x.equals(other._x) && _y.equals(other._y) && _z.equals(other._z);
     }
 
+    /**the function return string of the point
+     * @return string with 3 coordinate of the point
+     */
     @Override
     public String toString() {
         return "pt: (" + _x + " " + _y + " " + _z + ")";
@@ -68,9 +105,9 @@ public final class Point3D {
     /************** Operations***************/
 
     /**
-     * @param other- another point
-     * @return the vector received from subtract a point from our point
-     * @throws Exception
+     * the vector received from subtract a point from our point
+     * @param other- point
+     * @return vector
      **/
 
     public Vector subtract(Point3D other) {
@@ -78,14 +115,16 @@ public final class Point3D {
     }
 
     /**
-     * @param vector - a vector
-     * @return the point received when adding the vector to the point
+     * the point received when adding the vector to the point
+     * @param vector -  vector
+     * @return new point3D
      */
     public Point3D add(Vector vector) {
         return new Point3D(_x.add(vector.getHead()._x), _y.add(vector.getHead()._y), _z.add(vector.getHead()._z));
     }
 
     /**
+     * the distane between the points
      * @param p2- other point
      * @return the distane between the points
      */
@@ -95,7 +134,6 @@ public final class Point3D {
 
     /**
      * The function calculates squared distance to other point
-     *
      * @param p2- other point
      * @return the squared distance
      */
