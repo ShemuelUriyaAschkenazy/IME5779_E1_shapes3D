1cf55207b727261cb60629996d3c8f77352866c2
shimonbalsam <46459891+shimonbalsam@users.noreply.github.com>
2019-05-02 16:55:41 +0300

    javadoc at geometries all with triangle

--- src/geometries/Triangle.java
+++ src/geometries/Triangle.java
@@ -6,6 +6,9 @@ import primitives.Vector;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * triangle class contains 3 point3D the corners of triangle and addition produce vector who is the normal to the triangle
+ */
 public class Triangle extends Plane{
 
     Point3D p1;
@@ -13,8 +16,15 @@ public class Triangle extends Plane{
     Point3D p3;
 
 
-    /********** Constructors ***********/
+    /* ********* Constructors ***********/
 
+    /**
+     * constructor that receive 3 point the corners of triangle, applies it to new triangle and produce the normal vector to triangle
+     *
+     * @param p1 point3D
+     * @param p2 point3D
+     * @param p3 point3D
+     */
     public Triangle(Point3D p1, Point3D p2, Point3D p3){
         super(p1, p2 ,p3);
         this.p1 = p1;
@@ -23,27 +33,35 @@ public class Triangle extends Plane{
 
     }
 
-    /************** Getters/Setters *******/
+    /**
+     * function return point A of triangle
+     *
+     * @return point A of triangle
+     */
+    /* ************* Getters/Setters *******/
     public Point3D getP1() {
         return p1;
     }
 
+    /**
+     * function return point B of triangle
+     *
+     * @return point B of triangle
+     */
     public Point3D getP2() {
         return p2;
     }
 
+    /**
+     * function return point C of triangle
+     *
+     * @return point C of triangle
+     */
     public Point3D getP3() {
         return p3;
     }
 
-    @Override
-    public String toString() {
-        return "Triangle{" +
-                "p1=" + p1 +
-                ", p2=" + p2 +
-                ", p3=" + p3 +
-                '}';
-    }
+    
 
     /**
      * find the intersections points between a ray and the triangle
