66ed6ed3f77c156c70d82e7deceb6d1f49975872
שמואל אוריה אשכנזי <uriyaas@gmail.com>
2019-05-29 17:01:49 +0300

    java docs

--- src/geometries/Geometry.java
+++ src/geometries/Geometry.java
@@ -12,16 +12,41 @@ abstract public class Geometry implements Intersectable {
     protected Color _emission;
     protected Material _material;
 
+    /**
+     * constructor
+     *
+     * @param _emission emission light
+     * @param material  material object (values of material type)
+     */
     public Geometry(Color _emission, Material material) {
         //shallow copy indeed, but not bad because there is no set function.
         this._emission = _emission;
         _material = material;
     }
 
+    /**
+     * getNormal function to get the normal vector from a point on a geometry
+     *
+     * @param point3D a point on geometry
+     * @return the normal vector
+     */
     abstract public Vector getNormal(Point3D point3D);
 
+    /**
+     * getEmission function to get the emission light on the geometry
+     *
+     * @return emission light
+     */
     public Color getEmission() {
         return _emission;
     }
-    public Material getMaterial() { return _material; }
+
+    /**
+     * getMaterial function to get the material values of the geometry
+     *
+     * @return the material (object with material values)
+     */
+    public Material getMaterial() {
+        return _material;
+    }
 }
